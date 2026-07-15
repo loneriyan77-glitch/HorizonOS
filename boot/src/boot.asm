4261c9fcd9106dbed9ed8a78b2aed13b54b060cc
Riyan <loneriyan77@gmail.com>
2026-07-15 23:59:29 +0530

    Milestone 5: Print first character

--- boot/src/boot.asm
+++ boot/src/boot.asm
@@ -1,13 +1,18 @@
 ; ============================================
 ; HorizonOS Bootloader
 ; Author: Riyan
-; Day 3
 ; ============================================
 
 [BITS 16]
 [ORG 0x7C00]
 
 start:
+    mov ah, 0x0E      ; BIOS teletype service
+    mov al, 'A'       ; Character to print
+    int 0x10          ; Ask BIOS to print it
 
 hang:
     jmp hang
+
+times 510-($-$$) db 0
+dw 0xAA55
